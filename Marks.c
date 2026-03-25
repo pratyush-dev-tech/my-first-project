01a4e7129aeab1df6a169e7a244a799e0a2a076c
Pratyush Chatterjee <gaurabnht2006@gmail.com>
2026-03-25 21:24:41 +0530

    Add files via upload

--- Marks.c
+++ Marks.c
@@ -4,30 +4,30 @@ int main()
     char sub1, sub2;
     int marks;
 
-    printf("Choose subject 1 (m for math, s for science): ");
-    scanf(" %c", &sub1);
+          printf("Choose subject 1 (m for math, s for science): ");
+          scanf(" %c", &sub1);
 
-    printf("Choose subject 2 (m for math, s for science): ");
-    scanf(" %c", &sub2);
+               printf("Choose subject 2 (m for math, s for science): ");
+               scanf(" %c", &sub2);
 
-    if((sub1=='m' && sub2=='s') || (sub1=='s' && sub2=='m'))
-    {
-        marks = 45;
-    }
-    else if(sub1=='m' || sub2=='m')
-    {
-        marks = 30;
-    }
-    else if(sub1=='s' || sub2=='s')
-    {
-        marks = 20;
-    }
-    else
-    {
-        marks = 0;
-    }
+               if((sub1=='m' && sub2=='s') || (sub1=='s' && sub2=='m'))
+               {
+                     marks = 45;
+               }
+                     else if(sub1=='m' || sub2=='m')
+                   {
+                         marks = 30;
+                   }
+                          else if(sub1=='s' || sub2=='s')
+                       {
+                           marks = 20;
+                       }
+                            else
+                        {
+                            marks = 0;
+                        }
 
-    printf("Marks = %d\n", marks);
+                             printf("Marks = %d\n", marks);
 
-    return 0;
+                            return 0;
 }
