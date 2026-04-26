aba7fa2a030c53956367afb75af6c34125b78b8e
Pratyush Chatterjee <gaurabnht2006@gmail.com>
2026-04-26 21:13:36 +0530

    Add files via upload

--- password.c
+++ password.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 int main() {
     char chars[] =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -10,19 +9,14 @@ int main() {
         "!@#$%^&*";
 
     int length, i;
-    
     printf("Enter password length: ");
     scanf("%d", &length);
-
     srand(time(0));
-
     printf("Generated Password: ");
-
     for(i = 0; i < length; i++) {
         int index = rand() % (sizeof(chars) - 1);
         printf("%c", chars[index]);
     }
-
     printf("\n");
 
     return 0;
